e7311c186e2f84cd252863389d5f5db1ffbf809d
ethan <zyx333@users.noreply.github.com>
2018-07-18 14:45:43 +0800

    first commit

---
+++ src/main/java/zyx/practise/springmvc/MVCTest.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2018, TP-Link Co.,Ltd.
+ * Author:  zhangyuxiang <zhangyuxiang@tp-link.com.cn>
+ * Created: 2018-07-16
+ */
+package zyx.practise.springmvc;
+
+public class MVCTest {
+}
