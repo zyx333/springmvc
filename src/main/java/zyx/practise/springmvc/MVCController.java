e8ea1bf3479db7fa405d68bbbcfee1b46e484201
ethan <zyx333@users.noreply.github.com>
2018-07-18 15:38:26 +0800

    using InternalResourceViewResolver to display content

---
+++ src/main/java/zyx/practise/springmvc/MVCController.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2018, TP-Link Co.,Ltd.
+ * Author:  zhangyuxiang <zhangyuxiang@tp-link.com.cn>
+ * Created: 2018-07-18
+ */
+package zyx.practise.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mvc")
+public class MVCController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "/index";
+    }
+
+}
