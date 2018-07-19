f221a40d2a5abf1c34a79ad77b0561c05ef11788
ethan <zyxthr@gmail.com>
2018-07-19 23:02:32 +0800

    add interceptor

---
+++ src/main/java/zyx/practise/springmvc/interceptor/MyInterceptor.java
@@ -0,0 +1,29 @@
+/**
+ * Created by ethan on 2018/7/19.
+ */
+
+package zyx.practise.springmvc.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        System.out.println("pre handler");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("post handler");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("after completion");
+    }
+}
