2ea068fe4f098c90a6625dbf6c0e6b6931dce051
ethan <zyx333@users.noreply.github.com>
2018-07-19 18:00:39 +0800

    add error controller

--- src/main/java/zyx/practise/springmvc/controller/ExceptionController.java
+++ src/main/java/zyx/practise/springmvc/controller/ExceptionController.java
@@ -7,12 +7,23 @@ package zyx.practise.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ExceptionController {
+    @ExceptionHandler
     public ModelAndView exceptionHandler(Exception ex) {
         ModelAndView mv = new ModelAndView("error");
-        
+        mv.addObject("exception", ex);
+        System.out.println("test exceptionHandler");
+        return mv;
+    }
+
+    @RequestMapping("/error")
+    public String error() {
+        int i = 5/0;
+        return "index";
     }
 }
